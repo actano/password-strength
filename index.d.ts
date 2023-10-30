12e3a1863bceb48964e1b0c6b92d898bbc5f4921
Daniel <daniel.mori@allex.ai>
2023-10-30 21:01:17 +0900

    PA-20759: Update `index.d.ts`

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,8 @@
 declare module "@rplan/password-strength" {
   export default function checkPassword(password: string): boolean
+  export function checkInputLength(type: string, input: string): boolean
+  export const InputType: {
+    Email: string,
+    Password: string
+  }
 }
